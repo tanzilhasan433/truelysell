a2d47b633497b1b7ce004cf202349e1d07a8f1c2
tanzilhasan433 <tanzilhasanlimon@gmail.com>
2025-09-29 01:46:33 +0600

    footer updated

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
 export const metadata = {
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
       <body className={` antialiased`}>
         <Header />
         <div className="my-20"> {children}</div>
+        <Footer />
       </body>
     </html>
   );
